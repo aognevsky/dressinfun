31210dc3e6c5ca23b1277084cb808f5b4d83cd80
Andrey Ognevsky <ognevsky@gmail.com>
2009-04-15 23:07:13 +0200

    Добавил плагины role и auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 ActionController::Routing::Routes.draw do |map|
+	map.resources :users
+  map.resource :session
+	
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.register '/register', :controller => 'users', :action => 'create'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+
+
 	map.root :controller => 'home'
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
